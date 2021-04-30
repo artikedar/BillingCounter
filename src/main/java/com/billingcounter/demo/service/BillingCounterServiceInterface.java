a8814a24a312ffcad53906aefcbea9fb068a494a
Arti <artikedar@gmail.com>
2021-04-30 17:52:23 +0530

    Created the Recipe Application

--- src/main/java/com/billingcounter/demo/service/BillingCounterServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.billingcounter.demo.service;
-
-import com.billingcounter.demo.data.PurchasedItems;
-import com.billingcounter.demo.data.TotalBill;
-
-public interface BillingCounterServiceInterface {
-
-	public TotalBill getItemizedBill(PurchasedItems input);
-}
